0c093bb8bc8e9ad40d6436e7224e794d23743823
bulldogBoy <yarik_kosmi4eskiy@proton.me>
2025-11-15 15:20:35 +0200

    AbstractPathBuilder class has been added.It responsible for creating correct path to photo in other classes

---
+++ AbstractPathBuilder.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaGridPoster
+{
+    public abstract class AbstractPathBuilder
+    {
+        public string? PathToImage { get; private set; }
+        protected abstract void SetPathMessage();
+        protected void SetPath()
+        {
+            SetPathMessage();
+            string? pathToImage = Console.ReadLine()?.Trim('"');
+            if (pathToImage is not null && PathValidate(pathToImage))
+            {
+                PathToImage = pathToImage;
+            }
+            else
+            {
+                SetPath();
+            }
+        }
+        private bool PathValidate(string path)
+        {
+            var format = path.Split(".");
+            if (format.Length > 0 && Enum.TryParse<FormatsEnum>(format.Last<string>(), true, out _) && File.Exists(path)) 
+            {
+                return true;
+            }
+            return false;
+        }
+
+        protected AbstractPathBuilder()
+        {
+            SetPath();
+        }
+    }
+}
