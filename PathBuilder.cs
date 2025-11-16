6b8d8ebf6d774044c83a5d0210bb12a410dd52d3
bulldogBoy <yarik_kosmi4eskiy@proton.me>
2025-11-16 20:24:07 +0200

    Added PathBuilder class it implement AbstractPathBuilder

---
+++ PathBuilder.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InstaGridPoster
+{
+    public class PathBuilder : AbstractPathBuilder
+    {
+        protected override void SetPathMessage()
+        {
+            Console.WriteLine("Enter the path to the photo");
+        }
+
+        protected override void SetPathSaveMessage()
+        {
+            Console.WriteLine("Specify the path to the folder where you want to save the poster.\r\n\r\n*Press Enter to save in the original photo's directory.*");
+        }
+    }
+}
