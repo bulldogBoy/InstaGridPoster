735016dd6e59e7de957eb835b430c6e7df1764f4
bulldogBoy <yarik_kosmi4eskiy@proton.me>
2025-11-16 20:44:13 +0200

    Initialize application in main method

--- Program.cs
+++ Program.cs
@@ -1,11 +1,21 @@
-﻿namespace InstaGridPoster
+﻿using System.Diagnostics;
+using System.IO;
+
+namespace InstaGridPoster
 {
     class Program
     {
-       
         static void Main(string[] args)
         {
-          
+           var pathBuilder = new PathBuilder();
+           var ffmpegArgumentsBuilder = new FfmpegArgumentsBuilder(pathBuilder.PathToImage!,pathBuilder.PathToSave!);
+           var ffmpegProcessSetup = new ProcessStartInfo()
+            {
+                FileName = "ffmpeg.exe",
+                Arguments = ffmpegArgumentsBuilder.BuildArgs(),
+                RedirectStandardError = true
+            };
+            Process.Start(ffmpegProcessSetup);
         }
     }
 }
